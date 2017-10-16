2c7f4619cfdae87224e02dd412615ac5d462200b
xu7103224 <xu7103224@126.com>
2017-10-16 17:12:45 +0800

    update Rotator

--- PubgMem/PubgMem/PUBG_Engine_structs.h
+++ PubgMem/PubgMem/PUBG_Engine_structs.h
@@ -43,11 +43,20 @@ public:
 	}
 };
 
-class Rotator {
+class Rotator
+{
 public:
-	unsigned char unknownchar[0xC];
+	/** Rotation around the right axis (around Y axis), Looking up and down (0=Straight Ahead, +Up, -Down) */
+	float Pitch;
+
+	/** Rotation around the up axis (around Z axis), Running in circles 0=East, +North, -South. */
+	float Yaw;
+
+	/** Rotation around the forward axis (around X axis), Tilting your head, 0=Straight, +Clockwise, -CCW. */
+	float Roll;
 
 };
+
 
 // Enum Engine.ENetRole
 enum class ENetRole
